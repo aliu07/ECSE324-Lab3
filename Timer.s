c6cefa5d54c3ca6cc0f8b9c98f6c6ccf23cfbe43
Alexander Liu <alexander_l_22@hotmail.com>
2024-03-19 14:28:02 -0400

    Completed timer drivers

--- Timer.s
+++ Timer.s
@@ -1,6 +1,6 @@
 .global _start
 
-.equ TIM_ADDR, 0xFFFEC600
+.equ TIMER_ADDR, 0xFFFEC600
 
 _start:
 	MOV A1, #2000000 // Instantiate load value for timer
@@ -14,15 +14,29 @@ _start:
 // A2: Configuration bits
 ARM_TIM_config_ASM: 
 	PUSH {LR}
-	
+	LDR A3, =TIMER_ADDR // Load base address of timer
+	STR A1, [A3] // Store load value into timer
+	STR A2, [A3, #8] // Store control value of timer (Base address + 8)
 	POP {LR}
 	BX LR
 
 // This subroutine returns the “F” value (0x00000000 or 0x00000001) from 
 // the ARM A9 private timer interrupt status register.
 ARM_TIM_read_INT_ASM:
+	PUSH {LR}
+	LDR A2, =TIMER_ADDR // Load base address into A2
+	LDR A1, [A2, #12] // Load control register value into A1
+	AND A1, A1, #0x00000001 // Keep only F bit which is MSB
+	POP {LR}
+	BX LR
 
 // This subroutine clears the “F” value in the ARM A9 private timer 
 // interrupt status register. The F bit can be cleared to 0 by writing 
 // a 0x00000001 to the interrupt status register.
 ARM_TIM_clear_INT_ASM:
+	PUSH {LR}
+	LDR A2, =TIMER_ADDR // Load base address into A2
+	MOV A1, #0 // Move 0 into A1 to clear interrupt bit F in timer
+	STR A1, [A2] // Write value in memory
+	POP {LR}
+	BX LR
